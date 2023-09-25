fec8d86795558409456237a50c52f9c4ca479443
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-25 13:47:09 -0400

    update

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.scss';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout'; // Make sure 'layout' is correctly capitalized
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />} /> {/* Use 'Layout' as the JSX element */}
+      </Routes>
     </>
   );
 }
