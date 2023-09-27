15f2cd10b3e1c0b14387f6605ce41f17090fc6c6
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-27 14:25:33 -0400

    update

--- react-portfolio/src/index.js
+++ react-portfolio/src/index.js
@@ -1,27 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client'; // Updated import
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')); // Updated usage
+root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />
+      <App />
     </BrowserRouter>
-    
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
