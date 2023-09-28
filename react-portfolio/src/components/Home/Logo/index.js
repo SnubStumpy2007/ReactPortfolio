eef73ddf8394e7ec486ccb8c9acc3579ca753f8d
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-28 16:57:50 -0400

    update

--- react-portfolio/src/components/Home/Logo/index.js
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -2,6 +2,12 @@ import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
 
 const Logo = () => {
+
+    const bgRef = useRef();
+    const outlineLogoRef = useRef();
+    const solidLogoRef=useRef();
+
+
      return (
         <div className='logo-container'>
             <img className='solid-logo' src={LogoS} alt="S" />
