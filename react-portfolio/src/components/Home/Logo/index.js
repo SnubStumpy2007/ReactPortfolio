3e197acb4d479c8f8c9de9cc23864afc01c63df4
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-28 19:45:40 -0400

    update

--- react-portfolio/src/components/Home/Logo/index.js
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -1,5 +1,8 @@
 import './index.scss'
 import LogoS from '../../../assets/images/logo-s.png'
+import { useEffect, useRef } from 'react';
+import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import gsap from 'gsap-trial'
 
 const Logo = () => {
 
@@ -7,10 +10,34 @@ const Logo = () => {
     const outlineLogoRef = useRef();
     const solidLogoRef=useRef();
 
+    useEffect(() => {
+        gsap.registerPlugin(DrawSVGPlugin)
+
+        gsap.timeline().to(bgRef.current, {
+            duration: 1,
+            opacity: 1,
+        })
+        .from(outlineLogoRef.current, {
+            drawSVG: 0,
+            duration: 20,
+        })
+    })
+
+    gsap.from.To(
+        solidLogoRef.current, {
+            opacity: 0,
+        },
+        {
+            opacity: 1,
+            delay: 4,
+            duration: 4,
+        }
+    )
+
 
      return (
         <div className='logo-container' ref={bgRef}>
-            <img className='solid-logo' src={LogoS} alt="S" />
+            <img ref={solidLogoRef} className='solid-logo' src={LogoS} alt="S" />
             <svg
         width="559pt"
         height="897pt"
