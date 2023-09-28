968d6811513fc14f3ee9067b100081e8330f12ab
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-27 20:18:25 -0400

    update

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,14 +1,20 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss'
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['l', '0', 'b', 'd', 'a', 'n']
+    const jobArray = ['w', 'e', 'b', '', 'd','e','v','e','l','o','p','e','r', '.']
+
     return (
         <div className='container home-page'>
             <div className="text-zone">
                 <h1>Hi, <br /> I'm </h1>
                 <img src={LogoTitle} alt='Developer' />
-                lobodon
+                <AnimatedLetters letterClass={letterClass}
+                strArray={nameArray} idx={15} />
                 <br />
                 web developer
             </div>
