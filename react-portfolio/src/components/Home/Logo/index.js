2e8d370bd5e6cc2ff05721bf47bc2b27f6bb265a
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-28 19:55:10 -0400

    update

--- react-portfolio/src/components/Home/Logo/index.js
+++ react-portfolio/src/components/Home/Logo/index.js
@@ -1,38 +1,36 @@
-import './index.scss'
-import LogoS from '../../../assets/images/logo-s.png'
+import './index.scss';
+import LogoS from '../../../assets/images/logo-s.png';
 import { useEffect, useRef } from 'react';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap-trial'
+import { DrawSVGPlugin } from 'gsap/dist/DrawSVGPlugin';
+import { gsap } from 'gsap';
 
 const Logo = () => {
 
     const bgRef = useRef();
     const outlineLogoRef = useRef();
-    const solidLogoRef=useRef();
-
+    const solidLogoRef = useRef();
+  
     useEffect(() => {
-        gsap.registerPlugin(DrawSVGPlugin)
-
+        gsap.registerPlugin(DrawSVGPlugin);
+      
         gsap.timeline().to(bgRef.current, {
-            duration: 1,
-            opacity: 1,
-        })
-        .from(outlineLogoRef.current, {
-            drawSVG: 0,
-            duration: 20,
-        })
-    })
-
-    gsap.from.To(
-        solidLogoRef.current, {
-            opacity: 0,
-        },
-        {
+          duration: 1,
+          opacity: 1,
+        }).from(outlineLogoRef.current, {
+          drawSVG: 0,
+          duration: 20,
+        });
+      
+        gsap.fromTo(
+          solidLogoRef.current,
+          { opacity: 0 },
+          {
             opacity: 1,
             delay: 4,
             duration: 4,
-        }
-    )
+          }
+        );
+      }, []);
 
 
      return (
