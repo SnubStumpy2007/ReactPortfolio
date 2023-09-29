8718a2384b0edc4592e4d26641bac6ba930010a2
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-28 22:39:22 -0400

    cleaning up

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,28 +1,32 @@
-import { Link } from 'react-router-dom';
-import LogoTitle from '../../assets/images/logo-s.png';
+import { useEffect, useState } from 'react'
+//import { Link } from 'react-router-dom'
+//import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
+import Sidebar from '../Sidebar'
 import './index.scss'
-import AnimatedLetters from '../AnimatedLetters';
-import React, { useEffect, useState } from 'react'; // Import useState from React
-import Logo from './Logo/index';
+
+
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['l', '0', 'b', 'd', 'a', 'n']
-    const jobArray = ['w', 'e', 'b', '', 'd','e','v','e','l','o','p','e','r', '.']
-
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['t', 'y', 'p', 'i', 'n', 's', 'k', 'i'];
+    const jobArray = ['w', 'e', 'b', '', 'd','e','v','e','l','o','p','e','r', '.'];
 
     useEffect(() => {
         const timeoutId = setTimeout(() => {
-            setLetterClass('text-animate-hover')
+            setLetterClass('text-animate-hover');
         }, 4000);
     
-        // Return a cleanup function to clear the timeout if the component unmounts
         return () => clearTimeout(timeoutId);
     }, []);
     
-
     return (
+        
         <div className='container home-page'>
+          <Sidebar />
+
             <div className="text-zone">
                 <h1> 
                     <span className={letterClass}>H</span>
@@ -30,22 +34,18 @@ const Home = () => {
                     <br /> 
                     <span className={`${letterClass} _13`}>I</span>
                     <span className={`${letterClass} _14`}>'m</span>
-                    </h1>
+                </h1>
                 <img src={LogoTitle} alt='Developer' />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray} idx={15} />
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
                 <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray} idx={22} />
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
             </div>
-            <h2>Frontend Developer</h2>
-
-            <Link to="/contact" className='flat-button'>Contact me</Link>
+            {/* <h2>Frontend Developer</h2> */}
+            {/* <Link to="/contact" className='flat-button'>About Me</Link> */}
             <Logo />
+            
         </div>
-    )
-
-
+    );
 }
 
-export default Home
+export default Home;
