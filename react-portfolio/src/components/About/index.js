b36fe3abfa21e365b5e89e8552a52409fac4ff19
SnubStumpy2007 <joshua192007@gmail.com>
2023-09-30 16:35:06 -0400

    update

--- react-portfolio/src/components/About/index.js
+++ react-portfolio/src/components/About/index.js
@@ -1,12 +1,27 @@
 import 'index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react';
 
 
 const About = () => {
+
+    const [letterClass, setLetterClass] = useState('text-animate');
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    
+        return () => clearTimeout(timeoutId);
+    }, []);
+
+    return (
+        <>
     <div className="container about-page">
         <div className="text-zone">
             <h1>
                 <AnimatedLetters
+                letterClass={letterClass}
                 strArray={['A','b','o','u','t', '', 'M','e']} idx={15} />
             </h1>
 
@@ -23,7 +38,33 @@ const About = () => {
             I'm always open to collaboration and excited to work on projects that allow me to leverage my expertise in React to build innovative and impactful web applications.
             </p>
         </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
     </div>
+    <Loader type="pacman" />
+    </>
+    )
 }
 
 
